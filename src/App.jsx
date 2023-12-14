847530b311e1fd9d23482f1d7b4a7873f4cad094
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-12-14 10:38:08 +0900

    ㅎ

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./page/Main";
 import About from "./page/About";
+import Categories from "./page/Categories";
 
 function App() {
   return (
     <Routes>
-        <Route index element={<Main />} />
-        <Route path='/about' element={<About />} />
+      <Route index element={<Main />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/categories" element={<Categories />} />
     </Routes>
   );
 }
---
+++ src/page/Categories.jsx
@@ -0,0 +1,18 @@
+import Layout from "../layout/Layout";
+import styled from "styled-components";
+
+function Categories() {
+  return (
+    <Layout>
+      <Process>
+        <H2></H2>
+      </Process>
+    </Layout>
+  );
+}
+export default Categories;
+
+const Process = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
